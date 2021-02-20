5f071e6ccef8c0fb8573a2363b585b24507b16d5
stocrene <53317894+stocrene@users.noreply.github.com>
2021-02-20 16:57:19 +0100

    Kamera Klasse kann nun Daten Senden
    Senden hinzugefügt

--- app/src/main/java/projekt/monitor/Camera.java
+++ app/src/main/java/projekt/monitor/Camera.java
@@ -6,33 +6,25 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.*;
-import android.os.Bundle;
-
 import android.util.Log;
 import java.net.Socket;
 
 
-
-
 import projekt.monitor.ui.monitor.ButtonsFragment;
-import projekt.monitor.ui.monitor.MonitorViewModel;
 
 
 public class Camera
 {
-    private int pos;
     private Socket socket;
     private int tcpPort = 10000;
     private boolean socketRunning = false;
-    private int x = 0;
-    private int y = 0;
     private String ip;
     private final String LOG_TAG = ButtonsFragment.class.getSimpleName();
 
-    public Camera()
+    public Camera(String ip, int tcpPort)
     {
-        pos = 0;
-        ip = "192.168.1.0";    // IP-Adresse aus sharedPreferences auslesen (siehe MonitorFragment)
+        this.tcpPort = tcpPort;
+        this.ip = ip;
     }
 
     //Überträgt die gewünschte Soll-Position
@@ -43,11 +35,11 @@ public class Camera
         try {
             object.put("X", x);
             object.put("Y", y);
-            object.put("pos", "pos");
+            //object.put("pos", "pos");
 
             try {
                 byte[] data = object.toString().getBytes("utf-8");
-                send(data, ip);
+                new Send(data, ip).start();
             } catch (UnsupportedEncodingException e) {
                 //e.printStackTrace();
                 Log.e(LOG_TAG, "Failed to create data", e);
@@ -62,17 +54,35 @@ public class Camera
     }
 
     //Überträgt die Richtung (z.B. Pfeiltasten & Joystick)
-    public void sendDirection()
+    public void sendDirection(int x,int y)
     {
+        Log.d(LOG_TAG, "Send Direction");
+        final JSONObject object = new JSONObject();
+        try{
+            object.put("X", x);
+            object.put("Y", y);
+            //object.put("speed", "speed");
 
+            try {
+                byte[] data = object.toString().getBytes("utf-8");
+                Log.d(LOG_TAG, object.toString());
+                new Send(data, ip).start();
+            } catch (UnsupportedEncodingException e) {
+                //e.printStackTrace();
+                Log.e(LOG_TAG, "Failed to create data", e);
+            }
+
+
+        } catch(JSONException e){
+            Log.e(LOG_TAG, "Failed to create JSONObject", e);
+        }
     }
 
     //Fragt beim Raspberry aktuelle Position an
     public void requestPosition()
     {
 
-        x = 0;
-        y = 0;
+
     }
 
     //UNKLAR
@@ -81,36 +91,42 @@ public class Camera
     {
 
     }
+    
 
+}
 
+class Send extends Thread {
+    private String ip;
+    private byte[] data;
+    private final String LOG_TAG = ButtonsFragment.class.getSimpleName();
+    private Socket socket;
+    private int tcpPort = 10000;
+    private boolean socketRunning = false;
 
-    private void send(byte[] data, String  ip)
-    {
-        try {
-            socket = new Socket(ip, tcpPort);
-            socketRunning = true;
-
-            try
-            {
-                Log.d(LOG_TAG, "Try send data");
-                OutputStream output = socket.getOutputStream();
-                output.write(data);
-
-            }
-            catch (IOException e)
-            {
-                Log.e(LOG_TAG, "Failed to get socket OutputStream", e);
-            }
-
-        } catch (IOException e) {
-            //e.printStackTrace();
-            Log.e(LOG_TAG, "Socket error", e);
-        }
-
-
-
-
+    public Send(byte[] data, String ip ) {
+        this.ip = ip;
+        this.data = data;
     }
 
 
+    public void run() {
+        Log.d(LOG_TAG, "Dateien werden gesendet");
+        try {
+            socket = new Socket("192.168.1.14", 10000);
+            socketRunning = true;
+
+            try {
+                Log.d(LOG_TAG, "Try send data");
+                OutputStream output = socket.getOutputStream();
+                output.write(data);
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Failed to get socket OutputStream", e);
+            }
+
+            socket.close();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Socket error", e);
+        }
+
+    }
 }
